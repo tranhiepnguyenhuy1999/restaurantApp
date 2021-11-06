9dd446b2137080f815b2bf552b1dc0ab76c39d5b
nguyenlxagj <17520756@gm.uit.edu.vn>
2021-11-06 19:45:47 +0700

    Create UI for Food Management

--- restaurant_management/dashboard.Designer.cs
+++ restaurant_management/dashboard.Designer.cs
@@ -30,137 +30,139 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dashboard));
             this.sideNav = new System.Windows.Forms.Panel();
-            this.titleBar = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.btnFood = new System.Windows.Forms.Button();
-            this.btnCashOut = new System.Windows.Forms.Button();
-            this.btnBill = new System.Windows.Forms.Button();
-            this.btnEmployees = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnBill = new System.Windows.Forms.Button();
+            this.btnCashOut = new System.Windows.Forms.Button();
+            this.btnFood = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
             this.logo = new System.Windows.Forms.PictureBox();
+            this.titleBar = new System.Windows.Forms.Panel();
             this.title = new System.Windows.Forms.Label();
             this.workspace = new System.Windows.Forms.Panel();
             this.sideNav.SuspendLayout();
-            this.titleBar.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.panelLogo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.titleBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // sideNav
             // 
-            this.sideNav.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.sideNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(181)))));
             this.sideNav.Controls.Add(this.btnExit);
             this.sideNav.Controls.Add(this.btnEmployees);
             this.sideNav.Controls.Add(this.btnBill);
             this.sideNav.Controls.Add(this.btnCashOut);
             this.sideNav.Controls.Add(this.btnFood);
-            this.sideNav.Controls.Add(this.panel3);
+            this.sideNav.Controls.Add(this.panelLogo);
             this.sideNav.Dock = System.Windows.Forms.DockStyle.Left;
             this.sideNav.Location = new System.Drawing.Point(0, 0);
             this.sideNav.Name = "sideNav";
             this.sideNav.Size = new System.Drawing.Size(200, 681);
             this.sideNav.TabIndex = 0;
             // 
-            // titleBar
+            // btnExit
             // 
-            this.titleBar.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.titleBar.Controls.Add(this.title);
-            this.titleBar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.titleBar.Location = new System.Drawing.Point(200, 0);
-            this.titleBar.Name = "titleBar";
-            this.titleBar.Size = new System.Drawing.Size(1064, 55);
-            this.titleBar.TabIndex = 1;
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(181)))));
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(162)))), ((int)(((byte)(255)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExit.Location = new System.Drawing.Point(0, 340);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnExit.Size = new System.Drawing.Size(200, 60);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // panel3
+            // btnEmployees
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.panel3.Controls.Add(this.logo);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(200, 100);
-            this.panel3.TabIndex = 0;
+            this.btnEmployees.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(181)))));
+            this.btnEmployees.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmployees.FlatAppearance.BorderSize = 0;
+            this.btnEmployees.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(162)))), ((int)(((byte)(255)))));
+            this.btnEmployees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployees.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEmployees.Location = new System.Drawing.Point(0, 280);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnEmployees.Size = new System.Drawing.Size(200, 60);
+            this.btnEmployees.TabIndex = 4;
+            this.btnEmployees.Text = "Employee Management";
+            this.btnEmployees.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEmployees.UseVisualStyleBackColor = false;
+            // 
+            // btnBill
+            // 
+            this.btnBill.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(181)))));
+            this.btnBill.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBill.FlatAppearance.BorderSize = 0;
+            this.btnBill.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(162)))), ((int)(((byte)(255)))));
+            this.btnBill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBill.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBill.Location = new System.Drawing.Point(0, 220);
+            this.btnBill.Name = "btnBill";
+            this.btnBill.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnBill.Size = new System.Drawing.Size(200, 60);
+            this.btnBill.TabIndex = 3;
+            this.btnBill.Text = "Bill Management";
+            this.btnBill.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBill.UseVisualStyleBackColor = false;
+            this.btnBill.Click += new System.EventHandler(this.btnBill_Click);
+            // 
+            // btnCashOut
+            // 
+            this.btnCashOut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(181)))));
+            this.btnCashOut.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCashOut.FlatAppearance.BorderSize = 0;
+            this.btnCashOut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(162)))), ((int)(((byte)(255)))));
+            this.btnCashOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCashOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCashOut.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCashOut.Location = new System.Drawing.Point(0, 160);
+            this.btnCashOut.Name = "btnCashOut";
+            this.btnCashOut.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnCashOut.Size = new System.Drawing.Size(200, 60);
+            this.btnCashOut.TabIndex = 2;
+            this.btnCashOut.Text = "Cash Out";
+            this.btnCashOut.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCashOut.UseVisualStyleBackColor = false;
             // 
             // btnFood
             // 
+            this.btnFood.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(181)))));
             this.btnFood.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnFood.FlatAppearance.BorderSize = 0;
-            this.btnFood.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnFood.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(162)))), ((int)(((byte)(255)))));
             this.btnFood.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFood.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFood.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.btnFood.Location = new System.Drawing.Point(0, 100);
             this.btnFood.Name = "btnFood";
             this.btnFood.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
             this.btnFood.Size = new System.Drawing.Size(200, 60);
             this.btnFood.TabIndex = 1;
-            this.btnFood.Text = "Quản lý món ăn";
+            this.btnFood.Text = "Food Management";
             this.btnFood.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnFood.UseVisualStyleBackColor = true;
+            this.btnFood.UseVisualStyleBackColor = false;
+            this.btnFood.Click += new System.EventHandler(this.btnFood_Click);
             // 
-            // btnCashOut
+            // panelLogo
             // 
-            this.btnCashOut.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnCashOut.FlatAppearance.BorderSize = 0;
-            this.btnCashOut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
-            this.btnCashOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCashOut.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnCashOut.Location = new System.Drawing.Point(0, 160);
-            this.btnCashOut.Name = "btnCashOut";
-            this.btnCashOut.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
-            this.btnCashOut.Size = new System.Drawing.Size(200, 60);
-            this.btnCashOut.TabIndex = 2;
-            this.btnCashOut.Text = "Thanh toán";
-            this.btnCashOut.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCashOut.UseVisualStyleBackColor = true;
-            // 
-            // btnBill
-            // 
-            this.btnBill.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnBill.FlatAppearance.BorderSize = 0;
-            this.btnBill.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
-            this.btnBill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBill.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnBill.Location = new System.Drawing.Point(0, 220);
-            this.btnBill.Name = "btnBill";
-            this.btnBill.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
-            this.btnBill.Size = new System.Drawing.Size(200, 60);
-            this.btnBill.TabIndex = 3;
-            this.btnBill.Text = "Quản lý bill";
-            this.btnBill.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnBill.UseVisualStyleBackColor = true;
-            // 
-            // btnEmployees
-            // 
-            this.btnEmployees.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnEmployees.FlatAppearance.BorderSize = 0;
-            this.btnEmployees.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
-            this.btnEmployees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEmployees.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnEmployees.Location = new System.Drawing.Point(0, 280);
-            this.btnEmployees.Name = "btnEmployees";
-            this.btnEmployees.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
-            this.btnEmployees.Size = new System.Drawing.Size(200, 60);
-            this.btnEmployees.TabIndex = 4;
-            this.btnEmployees.Text = "Quản lý nhân viên";
-            this.btnEmployees.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnEmployees.UseVisualStyleBackColor = true;
-            // 
-            // btnExit
-            // 
-            this.btnExit.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnExit.FlatAppearance.BorderSize = 0;
-            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
-            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnExit.Location = new System.Drawing.Point(0, 340);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
-            this.btnExit.Size = new System.Drawing.Size(200, 60);
-            this.btnExit.TabIndex = 5;
-            this.btnExit.Text = "Thoát";
-            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(181)))));
+            this.panelLogo.Controls.Add(this.logo);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(200, 100);
+            this.panelLogo.TabIndex = 0;
             // 
             // logo
             // 
@@ -172,21 +174,33 @@
             this.logo.TabIndex = 0;
             this.logo.TabStop = false;
             // 
+            // titleBar
+            // 
+            this.titleBar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.titleBar.Controls.Add(this.title);
+            this.titleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titleBar.Location = new System.Drawing.Point(200, 0);
+            this.titleBar.Name = "titleBar";
+            this.titleBar.Size = new System.Drawing.Size(1064, 55);
+            this.titleBar.TabIndex = 1;
+            // 
             // title
             // 
-            this.title.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.title.AutoSize = true;
-            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.title.Location = new System.Drawing.Point(483, 9);
+            this.title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(181)))));
+            this.title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.title.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.title.Location = new System.Drawing.Point(0, 0);
             this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(100, 37);
+            this.title.Size = new System.Drawing.Size(1064, 55);
             this.title.TabIndex = 0;
-            this.title.Text = "label1";
+            this.title.Text = "Home";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // workspace
             // 
-            this.workspace.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.workspace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(236)))), ((int)(((byte)(255)))));
             this.workspace.Dock = System.Windows.Forms.DockStyle.Fill;
             this.workspace.Location = new System.Drawing.Point(200, 55);
             this.workspace.Name = "workspace";
@@ -201,15 +215,16 @@
             this.Controls.Add(this.workspace);
             this.Controls.Add(this.titleBar);
             this.Controls.Add(this.sideNav);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "dashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.dashboard_FormClosed);
             this.sideNav.ResumeLayout(false);
-            this.titleBar.ResumeLayout(false);
-            this.titleBar.PerformLayout();
-            this.panel3.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.titleBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -222,7 +237,7 @@
         private System.Windows.Forms.Button btnBill;
         private System.Windows.Forms.Button btnCashOut;
         private System.Windows.Forms.Button btnFood;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelLogo;
         private System.Windows.Forms.Panel titleBar;
         private System.Windows.Forms.PictureBox logo;
         private System.Windows.Forms.Label title;
---
+++ restaurant_management/food-management.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace restaurant_management
+{
+    public partial class foodManagementForm : Form
+    {
+        public foodManagementForm()
+        {
+            InitializeComponent();
+        }
+
+        private void foodManagementForm_Load(object sender, EventArgs e)
+        {
+            SetupDataGridView();
+            PopulateDataGridView();
+        }
+
+        private void SetupDataGridView()
+        {
+            foodsDataGridView.ColumnCount = 2;
+
+            foodsDataGridView.Columns[0].Name = "Name";
+            foodsDataGridView.Columns[1].Name = "Price";
+        }
+
+        private void PopulateDataGridView()
+        {
+            string[] row0 = { "Beef Noodles", "30000" };
+            string[] row1 = { "Tea", "5000" };
+
+            foodsDataGridView.Rows.Add(row0);
+            foodsDataGridView.Rows.Add(row1);
+        }
+
+        private void foodsDataGridView_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            idTextBox.Text = e.RowIndex.ToString();
+            nameTextBox.Text = foodsDataGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
+            priceTextBox.Text = foodsDataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+        }
+    }
+}
