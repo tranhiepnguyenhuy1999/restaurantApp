1dc08c3c05258cd7b4b4ae39728fd3745dcce2c4
tranhiepnguyenhuy1999 <tranhiepnguyenhuy1999@gmail.com>
2021-11-08 22:21:21 +0700

    update database

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ USE restaurantManagement;
 
 CREATE TABLE kind(
 	id INT(6) AUTO_INCREMENT PRIMARY KEY, 
-	kìnd_name VARCHAR(100) NOT NULL,
+	kind_name VARCHAR(100) NOT NULL,
 	from_age INT,
     to_age INT,
     create_date DATE
@@ -30,14 +30,12 @@ CREATE TABLE bill(
 
 CREATE TABLE bill_detail(
 	id INT(6) AUTO_INCREMENT , 
-	price FLOAT DEFAULT 0,
     amount int NOT NULL,
     id_food INT NOT NULL,
     id_bill INT NOT NULL,
-    create_date DATE NOT NULL,
     PRIMARY KEY(id),
-	FOREIGN KEY (id_bill)
-	REFERENCES bill(id)
+	FOREIGN KEY (id_bill) REFERENCES bill(id),
+	FOREIGN KEY (id_food) REFERENCES food(id)
 );
 
 CREATE TABLE user(
@@ -65,6 +63,7 @@ BEGIN
 END; $$
 DELIMITER $$
 DROP PROCEDURE IF EXISTS insertNewFood $$
+
 CREATE PROCEDURE insertNewFood(
 IN 	food_name NVARCHAR(100),
 IN 	price FLOAT,
@@ -78,28 +77,48 @@ BEGIN
 	(food_name, price, id_kind, create_date);
 END; $$
 DELIMITER ;
-
+DELIMITER $$
+DROP PROCEDURE IF EXISTS insertBillDetail $$
+CREATE PROCEDURE insertBillDetail(
+    IN amount INT,
+    IN id_food INT,
+    IN id_bill INT
+)
+BEGIN
+   INSERT INTO 
+	bill_detail(amount, id_food, id_bill)
+	VALUES	
+	(amount, id_food, id_bill);
+END; $$
+DELIMITER ;
 INSERT INTO 
 	kind(kind_name, create_date)
 	VALUES	
 	('Món ăn', '2021-10-20'),
 	('Tráng miệng', '2021-10-20'),
 	('Thức uống', '2021-10-20');
-    
 -- food
 CALL insertNewFood('Lẩu cá', 250000, 1, '2021-10-20');
 CALL insertNewFood('Thịt kho', 30000, 1, '2021-10-21');
 CALL insertNewFood('Canh chua cá kho tộ', 1000000, 1, '2021-10-21');
 CALL insertNewFood('Cơm tấm', 35000, 1, '2021-10-21');
 CALL insertNewFood('Cơm gà xói mỡ', 30000, 1, '2021-10-21');
-CALL insertNewFood('Pepsi', 1000, 3, '2021-10-21');
+CALL insertNewFood('Pepsi', 10000, 3, '2021-10-21');
 CALL insertNewFood('C2', 8000, 3, '2021-10-21');
 CALL insertNewFood('Rau câu', 12000, 2, '2021-10-21');
 CALL insertNewFood('Bánh mì Bò Kho', 45000, 1, '2021-10-21');
 CALL insertNewFood('Hủ tiếu mực', 60000, 1, '2021-10-21');
 -- user
 CALL insertUser('Như', 'Trần Thị','2019-05-15','2019-11-20');
-CALL insertUser('Huy', 'Trần','2019-08-01','2019-12-31');
-CALL insertUser( 'Thanh', 'Nguyễn Thị','2019-05-15','2019-11-20');
+CALL insertUser('Huy', 'Trần','2019-08-01','2019-12-31'); 
 
+-- bill
+INSERT INTO 
+	bill(total_money, total_amount, create_date)
+	VALUES	
+	(610000 , 21, '2021-10-20');
+
+CALL insertBillDetail (1, 1, 1);
+CALL insertBillDetail (10, 2, 1);
+CALL insertBillDetail (10, 6, 1);
 
