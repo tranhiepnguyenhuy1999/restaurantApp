9fd1cf83ba9b943bae28ff1d852d40416ec24ce2
tranhiepnguyenhuy1999 <tranhiepnguyenhuy1999@gmail.com>
2021-11-08 13:42:26 +0700

    update database

--- database.sql
+++ database.sql
@@ -2,6 +2,45 @@ DROP DATABASE restaurantManagement;
 CREATE DATABASE restaurantManagement;
 USE restaurantManagement;
 
+CREATE TABLE kind(
+	id INT(6) AUTO_INCREMENT , 
+	name VARCHAR(100) NOT NULL,
+	from_age INT,
+    to_age INT,
+    create_date DATE
+);
+
+CREATE TABLE food(
+	id INT(6) AUTO_INCREMENT , 
+	name VARCHAR(100) NOT NULL,
+	price FLOAT DEFAULT 0,
+    id_kind INT, 
+    create_date DATE NOT NULL,
+    PRIMARY KEY(id),
+	FOREIGN KEY (id_kind)
+	REFERENCES kind(id)
+);
+
+CREATE TABLE bill(
+	id INT(6) AUTO_INCREMENT , 
+	total_money FLOAT DEFAULT 0,
+    total_amount int NOT NULL,
+    create_date DATE NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE bill_detail(
+	id INT(6) AUTO_INCREMENT , 
+	price FLOAT DEFAULT 0,
+    amount int NOT NULL,
+    id_food INT NOT NULL,
+    id_bill INT NOT NULL,
+    create_date DATE NOT NULL,
+    PRIMARY KEY(id),
+	FOREIGN KEY (id_bill)
+	REFERENCES bill(id)
+);
+
 CREATE TABLE user(
 	id INT AUTO_INCREMENT , 
 	first_name VARCHAR(100) NOT NULL,
@@ -10,6 +49,23 @@ CREATE TABLE user(
     create_date DATE NOT NULL,
     PRIMARY KEY(id)
 );
+
+DELIMITER $$
+CREATE PROCEDURE insertUser(
+IN 	first_name_user NVARCHAR(100),
+IN 	last_name_user NVARCHAR(100),
+IN birthday_user DATE,
+IN create_date_user DATE
+)
+BEGIN
+   INSERT INTO 
+	user(first_name, last_name, birthday, create_date)
+	VALUES
+	(first_name_user, last_name_user, birthday_user, birthday_user);
+END; $$
+
+ CALL insertUser('Thanh', 'Nguyễn Thị','2019-05-15','2019-11-20');
+
 INSERT INTO 
 	user(first_name, last_name, birthday, create_date)
 VALUES
--- restaurant_management/DAO/userDAO.cs
+++ restaurant_management/DAO/userDAO.cs
@@ -20,11 +20,21 @@ namespace restaurant_management.DAO
         private userDAO() { }
 
         public DataTable getUserList ()
-         {
+        {
             DataTable data;
             string query = "select * from user";
             data = DataProvider.Instance.ExecuteQuery(query);
             return data;
+        }
+
+        public int insertNewUser(string firstName, string lastName, string birthDay, string createDate) {
+            int result = 0;
+            string query = " CALL insertUser ( @first_name , @last_name , @birthday , @create_date )";
+
+            result = DataProvider.Instance.ExecuteNonQuery(query, new object[]{ firstName, lastName, birthDay, createDate});
+
+            return result;
+                
         }
     }
 }
