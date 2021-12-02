03171f60b8e6a28eea58d7a564c510be095f9ed2
Trungggggggg <20520336@gm.uit.edu.vn>
2021-12-02 15:13:11 +0700

    update user + bill form
    Sort by from List->Combo box
    Checked Detail Bill
    Add update_user function
    Add add_user function

---
+++ restaurant_management/add_user.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace restaurant_management
+{
+    public partial class add_user : Form
+    {
+        public add_user()
+        {
+            InitializeComponent();
+        }
+    }
+}
