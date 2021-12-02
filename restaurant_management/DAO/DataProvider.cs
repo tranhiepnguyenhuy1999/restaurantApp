2cd46726776078b343b1f613dc4cb42c9ac6b372
NVLuong <86339899+nvluong123@users.noreply.github.com>
2021-12-02 09:10:50 +0700

    login

--- restaurant_management/DAO/DataProvider.cs
+++ restaurant_management/DAO/DataProvider.cs
@@ -22,7 +22,7 @@ namespace restaurant_management.DAO
 
         private DataProvider() { }
 
-        private string SQL_conection_string = "server=localhost;uid=root;" + "pwd=;database=restaurantManagement;Convert Zero Datetime=True";
+        private string SQL_conection_string = "server=localhost;uid=root;" + "pwd=root;database=restaurantManagement;Convert Zero Datetime=True";
 
         public DataTable ExecuteQuery(string query, object[] param = null)
         {
