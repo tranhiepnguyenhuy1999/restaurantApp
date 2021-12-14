2ad0b2ccfd61eedc6d84cf8a541f87fa71946f52
tranhiepnguyenhuy1999 <tranhiepnguyenhuy1999@gmail.com>
2021-12-14 20:40:27 +0700

    update

--- restaurant_management/DAO/foodDAO.cs
+++ restaurant_management/DAO/foodDAO.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
 
 namespace restaurant_management.DAO
 {
@@ -48,7 +50,69 @@ namespace restaurant_management.DAO
                 Create_date = m.Field<DateTime>("create_date")
             }).ToList();
         }
+        public void exportExcelListFood(string name = "")
+        {
+            string query;
+            if (!name.Equals(""))
+            {
+                query = "select * from food where food_name=" + '"' + name + '"';
+            }
+            else query = "select * from food";
 
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+            try
+            {
+                if (data == null || data.Columns.Count == 0)
+                    throw new Exception("ExportToExcel: Null or empty input table!\n");
+
+                // load excel, and create a new workbook
+                var excelApp = new Excel.Application();
+                excelApp.Workbooks.Add();
+
+                // single worksheet
+                Excel._Worksheet workSheet = excelApp.ActiveSheet;
+
+                // column headings
+                for (var i = 0; i < data.Columns.Count; i++)
+                {
+                    workSheet.Cells[1, i + 1] = data.Columns[i].ColumnName;
+                }
+
+                // rows
+                for (var i = 0; i < data.Rows.Count; i++)
+                {
+                    // to do: format datetime values before printing
+                    for (var j = 0; j < data.Columns.Count; j++)
+                    {
+                        workSheet.Cells[i + 2, j + 1] = data.Rows[i][j];
+                    }
+                }
+
+                // check file path
+                if (!string.IsNullOrEmpty(""))
+                {
+                    try
+                    {
+                        workSheet.SaveAs("excelFilePath");
+                        excelApp.Quit();
+                        MessageBox.Show("Excel file saved!");
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("ExportToExcel: Excel file could not be saved! Check filepath.\n"
+                                            + ex.Message);
+                    }
+                }
+                else
+                { // no file path is given
+                    excelApp.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ExportToExcel: \n" + ex.Message);
+            }
+        }
         public List<DTO.Food> getListFoodByName(string name)
         {
             return this.getListFood(name);
